2e23256ee2287ac6da592e48a6483bb59007a7f1
Anilumar <anilskalyane@gmail.com>
2017-11-10 13:34:18 +0530

    Database name issue fixed

--- dbUserCreate.sh
+++ dbUserCreate.sh
@@ -1,54 +1,74 @@
+#!/bin/bash
+
 #Declare DB details variables
 echo "Please enter Host/IP"
 read DB_HOST
 
+echo "Please enter the database name or prefix (if matched with multiple then individual user will create)"
+read databasePrefix
+
 echo "Please enter root user MySQL password!"
 read rootpasswd
 
-printf "\n***** Start DB User Creation *****\n"
+echo
+printf "***** Start DB User Creation *****"
+echo 
 
-mysql -h $DB_HOST -u root -p${rootpasswd} -e "SHOW DATABASES LIKE 'rubicon_%';" | while read -r rcDatabase
+mysql -h $DB_HOST -u root -p${rootpasswd} -e "SHOW DATABASES LIKE 'f%';" | while read -r rcDatabase
 do
-MAINDB=$rcDatabase
+#MAINDB=$rcDatabase
+: ' 
+replace "-" with "_" for database username
+'
+MAINDB=${rcDatabase//_/}
+#MAINDB=${rcDatabase//-/_}
 
 # assign username
-USERNAME="$rcDatabase""AppAccess"
+USERNAME="$MAINDB""AppAccess"
 
 # create random password for APP USER
 PASSWDDB="$(openssl rand -base64 15)"
 
 # create application DB user access
-: '
-mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
-mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'%';"
+mysql -uroot -p${rootpasswd} -e "CREATE USER '${USERNAME}'@'%' IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${rcDatabase}.* TO '${USERNAME}'@'%';"
 mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-'
-echo "\n\n----- $rcDatabase DATABASE APP USER ACCESS -----\n" 
+
+echo
+echo
+echo "----- $rcDatabase DATABASE APP USER ACCESS -----"
+echo 
 
 echo "USERNAME: $USERNAME"
 echo "PASSWORD: $PASSWDDB" 
 
-echo "\n----- END APP USER ACCESS -----\n\n"
+echo
+echo "----- END APP USER ACCESS -----"
+echo
+echo
 
 # assign username
-USERNAME="$rcDatabase""DevAccess"
+USERNAME="$MAINDB""DevAccess"
 
 # create random password for DEV USER
 PASSWDDB="$(openssl rand -base64 15)"
 
 # create developer DB user access - only view access 
-: '
-mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@'%' IDENTIFIED BY '${PASSWDDB}';"
-mysql -uroot -p${rootpasswd} -e "GRANT SELECT PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'%';"
+mysql -uroot -p${rootpasswd} -e "CREATE USER ${USERNAME}@'%' IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p${rootpasswd} -e "GRANT SELECT PRIVILEGES ON ${rcDatabase}.* TO '${USERNAME}'@'%';"
 mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-'
 
-echo "\n----- $rcDatabase DATABASE DEV USER ACCESS -----\n"
+echo
+echo "----- $rcDatabase DATABASE DEV USER ACCESS -----"
+echo
 
 echo "USERNAME: $USERNAME" 
 echo "PASSWORD: $PASSWDDB" 
 
-echo "\n----- END DEV USER ACCESS -----\n\n"
+echo
+echo "----- END DEV USER ACCESS -----"
+echo
+echo
 
 done
 
